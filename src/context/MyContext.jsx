bb92aee9c92b18f2453de0fc5e2902980e087530
Mohammad-Yaseen69 <yaseenyaseenali69@gmail.com>
2024-12-26 18:33:54 +0500

    Making api call in context

--- src/context/MyContext.jsx
+++ src/context/MyContext.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react';
-import { createContext } from "react";
+// context/MyContext.js
+import React, { createContext, useState, useEffect } from "react";
+import useApi from "../Api/useApi";
 
-export const MyContext = createContext()
+export const MyContext = createContext();
 
 export const ContextProvider = ({ children }) => {
-    const [userData, setUserData] = useState(null)
-    const [paymentAddress, setPaymentAddress] = useState(null)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
+  const { data: userDataResponse, error: userError, loading: userLoading } = useApi("user/get-user-details/spoidy");
+  const { data: paymentDataResponse, error: paymentError, loading: paymentLoading } = useApi("payment-address/get-payment-address-public/spoidy");
 
-    return (
-        <MyContext.Provider value={{ userData, setUserData, paymentAddress, setPaymentAddress, loading, setLoading, error, setError }}>
-            {children}
-        </MyContext.Provider>
-    )
-}
+  const [userData, setUserData] = useState(null);
+  const [paymentAddress, setPaymentAddress] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (userDataResponse) {
+      setUserData(userDataResponse.userProfileInfo);
+    }
+    if (paymentDataResponse) {
+      setPaymentAddress(paymentDataResponse);
+    }
+    setLoading(userLoading || paymentLoading);
+    setError(userError || paymentError);
+  }, [userDataResponse, paymentDataResponse, userError, paymentError, userLoading, paymentLoading]);
+
+
+  return (
+    <MyContext.Provider value={{ userData, paymentAddress, loading, error, setUserData, setPaymentAddress }}>
+      {children}
+    </MyContext.Provider>
+  );
+};
